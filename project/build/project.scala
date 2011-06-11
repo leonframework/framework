890d270837d12ff21ce268ebcb0ca8de7a142c87
Roman Roelofsen <roman.roelofsen@gmail.com>
2011-06-11 16:50:02 +0200

    * Fixed license * License plugin will now run automatically

--- project/build/project.scala
+++ project/build/project.scala
@@ -79,14 +79,18 @@ class LeonParentProject(info: ProjectInfo) extends ParentProject(info) with Unpu
   // ===================================================================================================================
 
   trait LeonDefaults extends LicenseHeaders {
+    
     def licenseText =
-      """Copyright (c) 2010 WeigleWilczek and others.
+      """Copyright (c) 2011 WeigleWilczek and others.
 
 All rights reserved. This program and the accompanying materials
 are made available under the terms of the Eclipse Public License v1.0
 which accompanies this distribution, and is available at
 http://www.eclipse.org/legal/epl-v10.html
 """
+
+    override def compileAction = super.compileAction dependsOn formatLicenseHeaders
+
   }
 
   // ===================================================================================================================
