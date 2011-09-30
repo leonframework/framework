5c918694cd3f4d8ee0487b042955ae4682cc1c33
Alexander Thurow <thurow@weiglewilczek.com>
2011-09-30 14:36:30 +0200

    fixed wrong merge

--- leon-core/src/main/scala/io/leon/web/resources/resourcesModule.scala
+++ leon-core/src/main/scala/io/leon/web/resources/resourcesModule.scala
@@ -8,23 +8,22 @@
  */
 package io.leon.web.resources
 
-/*
- * Copyright (c) 2010 WeigleWilczek and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
 import javax.servlet._
 import http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 import com.google.inject.servlet.ServletModule
-import com.google.inject.{AbstractModule, Inject}
 import io.leon.web.WebUtils
 import io.leon.resources.{Resource, ResourceLoader}
+import org.slf4j.LoggerFactory
+import com.google.inject._
+import name.Names
 
 class ResourcesWebModule extends ServletModule {
   override def configureServlets() {
     install(new ResourcesModule)
+
+    bind(classOf[ExposedUrlCheckFilter]).in(Scopes.SINGLETON)
+    filter("/*").through(classOf[ExposedUrlCheckFilter])
+
     serve("/*").`with`(classOf[ResourcesServlet])
   }
 }
@@ -42,19 +41,8 @@ class ResourcesServlet @Inject()(resourceLoader: ResourceLoader) extends HttpSer
   private val welcomeFiles = List("index.html", "index.xhtml", "index.htm")
 
   override def service(req: HttpServletRequest, res: HttpServletResponse) {
-    val url = WebUtils.getRequestUrl(req)
-
-    val urlList = url.split('/').toList dropWhile { _ == "" }
-    urlList match {
-      case "leon" :: "jquery.js" :: Nil =>
-        doResource(req, res, "/leon/jquery-1.6.2.min.js")
-
-      case "leon" :: "knockout.js" :: Nil =>
-        doResource(req, res, "/leon/knockout-1.2.0.debug.js")
-
-      case xs =>
-        doResource(req, res, url)
-    }
+    val url = WebUtils.getRequestedResource(req)
+    doResource(req, res, url)
   }
 
   private def doResource(req: HttpServletRequest, res: HttpServletResponse, path: String) {
@@ -71,7 +59,7 @@ class ResourcesServlet @Inject()(resourceLoader: ResourceLoader) extends HttpSer
     resourceOption match {
       case Some(resource) => {
         setResponseContentType(req, res)
-        val stream = resource.getInputStream
+        val stream = resource.createInputStream()
         val buffer = new Array[Byte](1024)
         var bytesRead = 0
         while (bytesRead != -1) {
@@ -93,7 +81,23 @@ class ResourcesServlet @Inject()(resourceLoader: ResourceLoader) extends HttpSer
   
 }
 
-class InternalPathFilter(internalPaths: List[String]) extends Filter {
+object ExposedUrl {
+  def bind(binder: Binder, url: String) {
+    binder.bind(classOf[ExposedUrl]).annotatedWith(Names.named(url)).toInstance(new ExposedUrl(url))
+  }
+}
+
+class ExposedUrl(val urlRegex: String)
+
+class ExposedUrlCheckFilter @Inject()(injector: Injector) extends Filter {
+
+  import scala.collection.JavaConverters._
+
+  private val logger = LoggerFactory.getLogger(getClass.getName)
+
+  private def exposedUrls = injector.findBindingsByType(new TypeLiteral[ExposedUrl]() {}).asScala
+
+  private def exposedUrlsRegex = exposedUrls map { _.getProvider.get().urlRegex.r }
 
   def init(config: FilterConfig) {}
 
@@ -103,14 +107,16 @@ class InternalPathFilter(internalPaths: List[String]) extends Filter {
     val req = _req.asInstanceOf[HttpServletRequest]
     val res = _res.asInstanceOf[HttpServletResponse]
 
-    val isInternal = internalPaths exists { p => WebUtils.getRequestUrl(req).startsWith(p) }
-    if (isInternal) {
-      res.setStatus(403)
-      // TODO send page
-      return
-    } else {
+    val requestUrl = WebUtils.getRequestedResource(req)
+    val isPublic = exposedUrlsRegex exists { _.findFirstIn(requestUrl).isDefined }
+    if (isPublic) {
+      logger.debug("Requested exposed URL {}", requestUrl)
       chain.doFilter(_req, _res)
+    } else {
+      logger.debug("Requested *private* URL {}", requestUrl)
+      res.setStatus(403)
     }
   }
 
 }
+
