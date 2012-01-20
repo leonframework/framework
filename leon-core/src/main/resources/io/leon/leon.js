87774d232e737492d9038080385bf393bd8efb97
mricog <m.ricogomez@gmail.com>
2012-01-20 18:05:27 +0100

    removed obsolet asJavaObject method

--- leon-core/src/main/resources/io/leon/leon.js
+++ leon-core/src/main/resources/io/leon/leon.js
@@ -5,11 +5,6 @@ var leon = (function() {
     var guice = Packages.com.google.inject;
     var Names = Packages.com.google.inject.name.Names;
 
-    Object.prototype.asJavaObject = function(clazz) {
-        var converter = leon.inject(Packages.io.leon.javascript.Converter);
-        return converter.jsToJava(this, clazz);
-    };
-
     Object.prototype.properties = function() {
       var result = [];
       for (var property in this) {
