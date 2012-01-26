1eab5139162f6b56e7c046819cff720602ce2a4a
Roman Roelofsen <roman.roelofsen@gmail.com>
2012-01-26 16:51:19 +0100

    changes to leonjax sample

--- leon-samples/leonjax/src/main/resources/module.js
+++ leon-samples/leonjax/src/main/resources/module.js
@@ -1,12 +1,3 @@
-// module definition
-install(new Packages.io.leon.persistence.mongo.LeonMongoModule());
-install(new Packages.io.leon.resources.closure.ClosureTemplatesModule());
 
-// location of application files
-addLocation("./www");
-
-// server-side js files
 loadFile("/leonjax_server.js");
-
-// ajax support
 browser("leonJaxService").linksToServer()
