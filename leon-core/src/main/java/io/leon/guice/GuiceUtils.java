aa563a4af500c0999b405ff800f8ae23f0adef12
Roman Roelofsen <roman.roelofsen@gmail.com>
2012-02-08 14:27:17 +0100

    Added methods to GuiceUtils

--- leon-core/src/main/java/io/leon/guice/GuiceUtils.java
+++ leon-core/src/main/java/io/leon/guice/GuiceUtils.java
@@ -1,13 +1,24 @@
 package io.leon.guice;
 
-import com.google.inject.Binder;
+import com.google.inject.*;
 import com.google.inject.binder.ScopedBindingBuilder;
 import com.google.inject.name.Names;
+
+import java.util.List;
 
 public class GuiceUtils {
 
     public static <T> ScopedBindingBuilder bindClassWithName(Binder binder, Class<T> iface, Class<? extends T> clazz) {
-        return binder.bind(iface).annotatedWith(Names.named(clazz.getName())).to(clazz);
+        return binder.bind(getKeyWithInterfaceAndClassName(iface, clazz)).to(clazz);
+    }
+
+    public static <T>Key<T> getKeyWithInterfaceAndClassName(Class<T> iface, Class<? extends T> clazz) {
+        return Key.get(iface, Names.named(clazz.getName()));
+    }
+
+    public static <T> List<Binding<T>> getAllBindingsForType(Injector injector, Class<T> type) {
+        TypeLiteral<T> typeLiteral = Key.get(type).getTypeLiteral();
+        return injector.findBindingsByType(typeLiteral);
     }
 
 }
