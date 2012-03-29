17b4afd0fecfae4070133fd85993808c2532e7b7
Philipp Burgmer <mail@deflip.de>
2012-03-29 14:28:41 +0200

    extends leon-suite to show message with random int and a refresh link

--- leon-suite/src/main/java/com/acme/appname/DemoService.java
+++ leon-suite/src/main/java/com/acme/appname/DemoService.java
@@ -3,7 +3,8 @@ package com.acme.appname;
 public class DemoService {
 
     public String getA() {
-        return "Message from ServiceA";
+    	final int random = (int) (Math.random() * 1000);
+        return "Message with random int " + random + " from ServiceA";
     }
 
 }
--- leon-suite/src/main/webapp/index.client.coffee
+++ leon-suite/src/main/webapp/index.client.coffee
@@ -1,5 +1,8 @@
 
 @IndexController = getLeon().angular.utils.createController ($scope) ->
+  $scope.refresh = ->
     $scope.leon.service("/demoService", "getA").call (data) =>
         $scope.model.a = data
         $scope.$digest()
+
+  $scope.refresh()
