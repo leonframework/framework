df119eb98a0987820462e57ba0225a1939a016a5
Roman Roelofsen <romanroe@gmail.com>
2012-05-09 17:38:30 +0200

    Started work on unit tests for shiro

--- leon-core/src/test/java/io/leon/security/SecurityTest.java
+++ leon-core/src/test/java/io/leon/security/SecurityTest.java
@@ -1,16 +1,32 @@
 package io.leon.security;
 
+import io.leon.tests.browser.FirefoxLeonBrowserTester;
+import io.leon.tests.browser.LeonBrowserTester;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-@Test()
+@Test
 public class SecurityTest {
+
+    private LeonBrowserTester tester;
 
     @BeforeTest
     public void beforeTest() {
+        tester = new FirefoxLeonBrowserTester(new SecurityTestModule());
+        tester.start();
     }
 
-    public void val() {
+    @AfterTest()
+    public void afterTest() {
+        tester.stop();
+
+    }
+
+    public void val1() {
+    }
+
+    public void val2() {
     }
 
 }
--- leon-core/src/test/java/io/leon/security/SecurityTestModule.java
+++ leon-core/src/test/java/io/leon/security/SecurityTestModule.java
@@ -1,8 +1,23 @@
 package io.leon.security;
 
 import io.leon.LeonAppMainModule;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.realm.text.TextConfigurationRealm;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SecurityTestModule extends LeonAppMainModule {
+
+    @Override
+    public List<? extends Realm> getShiroRealms() {
+        TextConfigurationRealm realm = new TextConfigurationRealm();
+        StringBuilder lines = new StringBuilder();
+        lines.append("user1 = pass1, role1 \n");
+        lines.append("user2 = pass2, role2 \n");
+        realm.setUserDefinitions(lines.toString());
+        return Arrays.asList(realm);
+    }
 
     @Override
     protected void config() {
