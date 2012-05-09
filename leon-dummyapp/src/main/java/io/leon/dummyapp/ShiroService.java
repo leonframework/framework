759bc5501ce7c899cef4637009fdd042e46bad40
Roman Roelofsen <romanroe@gmail.com>
2012-05-09 14:35:50 +0200

    Use static imports

--- leon-dummyapp/src/main/java/io/leon/dummyapp/ShiroService.java
+++ leon-dummyapp/src/main/java/io/leon/dummyapp/ShiroService.java
@@ -1,14 +1,15 @@
 package io.leon.dummyapp;
 
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.annotation.RequiresRoles;
+
+import static org.apache.shiro.SecurityUtils.getSubject;
 
 @SuppressWarnings("UnusedDeclaration")
 public class ShiroService {
 
     public String auth(String username, String password) {
-        SecurityUtils.getSubject().login(new UsernamePasswordToken(username, password));
+        getSubject().login(new UsernamePasswordToken(username, password));
         return "auth done";
     }
 
@@ -17,7 +18,7 @@ public class ShiroService {
     }
 
     public String doPrivate1() {
-        SecurityUtils.getSubject().checkRole("role1");
+        getSubject().checkRole("role1");
         return "private1";
     }
 
