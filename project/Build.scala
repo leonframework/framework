2651aeb40aa300c6a12cfc735f6d50e99234ea09
Roman Roelofsen <romanroe@gmail.com>
2012-05-10 13:04:01 +0200

    new version 0.5.11

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import WebPlugin._
 
 object BuildSettings {
   val buildOrganization = "io.leon"
-  val buildVersion      = "0.5.10"
+  val buildVersion      = "0.5.11"
   val buildScalaVersion = "2.9.1"
   val buildDescription  = "JVM web framework for building data-driven web applications"
 
